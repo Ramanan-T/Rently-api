9de213bd394a1756778c09d1be472e6c9200d91f
ramanan <ramanan.t@rently.com>
2021-08-19 06:07:29 +0530

    done

---
+++ spec/requests/companies_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Companies", type: :request do
+
+  before do
+    @company = Company.create(company_name:"rr",company_contact:98123,company_address:"asdgdg")
+  end
+  puts "#{@company.company_name} #{@company.id}"
+ 
+  describe "GET /index" do
+    
+    
+  end
+end
